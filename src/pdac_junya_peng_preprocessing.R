fa2ba2e7e0e3d0a6c1f8e28e0a5e436bb548e86c
carcarre11 <carlos_carre11@hotmail.com>
2022-10-28 11:09:08 +0200

    add cell information and rebuild the paths to objects

--- src/pdac_junya_peng_preprocessing.R
+++ src/pdac_junya_peng_preprocessing.R
@@ -12,11 +12,11 @@ names(cfg) <- keys
 
 #said the working directory  
 data_folder <- cfg["working_dir"]
-data_folder <- paste0(data_folder, "/data")
+data_folder <- paste0(data_folder, "/metanalisis_bc")
 
 #load data
 load(paste0(data_folder,
-                               "/pdac_junya_peng.RData")
+    "/single_cell/raw/pdac_junya_peng/pdac_junya_peng.RData")
 )
 
 #check version of seurat object
@@ -24,8 +24,26 @@ Version(sc_Peng_PDAC)
 #version is 4.0.1
 
 
+#Add anotation to cells
+anot <- read.table(paste0(data_folder,
+    "/single_cell/raw/pdac_junya_peng/all_celltype.txt"),
+    sep = "\t",
+    header = T,
+    row.names = 1)
+
+sc_Peng_PDAC@meta.data <- merge(sc_Peng_PDAC@meta.data,
+    anot,
+    by = "row.names",
+    all = T)
+
+row.names(sc_Peng_PDAC@meta.data) <- sc_Peng_PDAC@meta.data$Row.names
+
+
 # Change default ident for later split and QC
 sc_Peng_PDAC@meta.data$orig.ident <- sc_Peng_PDAC@meta.data$Sample
+
+
+
 
 ## Split the merged obj
 sc_Peng_PDAC.samples <- Seurat::SplitObject(
@@ -39,7 +57,8 @@ names(sc_Peng_PDAC.samples) <- unique(sc_Peng_PDAC@meta.data$Sample)
 #Filtering and normalize data
 sc_Peng_PDAC_filtered <- lapply(X = sc_Peng_PDAC.samples,
                             FUN = filter_sc,    
-                            res_dir = paste0(data_folder, "/qc/pdac_junya_peng")
+                            res_dir = paste0(data_folder,
+                            "/single_cell/qc/pdac_junya_peng/")
                             )
 
 sc_Peng_PDAC_filtered <- lapply(X = sc_Peng_PDAC_filtered,
