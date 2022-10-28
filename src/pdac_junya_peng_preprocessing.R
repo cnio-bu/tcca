25fed565699697f88ab754f556ecf3de324e9058
carcarre11 <carlos_carre11@hotmail.com>
2022-10-28 10:02:21 +0200

    make preprocessing od fata

--- src/pdac_junya_peng_preprocessing.R
+++ src/pdac_junya_peng_preprocessing.R
@@ -12,17 +12,55 @@ names(cfg) <- keys
 
 #said the working directory  
 data_folder <- cfg["working_dir"]
-data_folder <- paste0(data_folder, "/single_cell")
+data_folder <- paste0(data_folder, "/data")
 
 #load data
 load(paste0(data_folder,
-                               "/raw/pdac_junya_peng/pdac_junya_peng.RData")
+                               "/pdac_junya_peng.RData")
 )
 
+#check version of seurat object
+Version(sc_Peng_PDAC)
+#version is 4.0.1
 
 
+# Change default ident for later split and QC
+sc_Peng_PDAC@meta.data$orig.ident <- sc_Peng_PDAC@meta.data$Sample
+
+## Split the merged obj
+sc_Peng_PDAC.samples <- Seurat::SplitObject(
+    object = sc_Peng_PDAC,
+    split.by = "Sample"
+    )
+
+names(sc_Peng_PDAC.samples) <- unique(sc_Peng_PDAC@meta.data$Sample)
 
 
+#Filtering and normalize data
+sc_Peng_PDAC_filtered <- lapply(X = sc_Peng_PDAC.samples,
+                            FUN = filter_sc,    
+                            res_dir = paste0(data_folder, "/qc/pdac_junya_peng")
+                            )
 
-data_folder
-"/home/ccarreterop/Documents/metanalisis_bc_todos/metanalisis_bc/single_cell/raw/"
+sc_Peng_PDAC_filtered <- lapply(X = sc_Peng_PDAC_filtered,
+                            FUN = normalize_and_scale
+)
+
+# Get rid of "NULL" samples, there are no cells left in these
+sc_Peng_PDAC_filtered[sapply(sc_Peng_PDAC_filtered, is.null)] <- NULL
+
+
+## Filter and subset tumor samples
+
+sc_Peng_PDAC_filtered <- sc_Peng_PDAC_filtered[names(sc_Peng_PDAC_filtered) 
+%in% paste0('T',1:24)]
+
+
+keep_malignant <- function(sc) {
+    sc_filtered <- subset(
+        x = sc,
+        subset = `characteristics..cell.type` == "Malignant"
+        )
+    return(sc_filtered)
+}
+
