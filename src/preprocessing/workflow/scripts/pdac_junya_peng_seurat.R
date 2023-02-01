8296ffbf6af5b8fe4da003dfce003e4bf5672ac2
Fran <66632994+FranSoriano@users.noreply.github.com>
2023-02-01 16:47:57 +0100

    Add pdac junya peng to workflow (#10)
    Co-authored-by: Santiago García Martín <51514997+SGMartin@users.noreply.github.com>

---
+++ src/preprocessing/workflow/scripts/pdac_junya_peng_seurat.R
@@ -0,0 +1,84 @@
+library("tidyverse")
+library("Seurat")
+
+## SNAKEMAKE I/O
+seurat_object_list <- snakemake@input[["object_list"]]
+celltype <- snakemake@input[["celltype"]]
+where_to_save <- snakemake@output[["seurat_list"]]
+
+## Load the source file
+load(seurat_object_list)
+
+## Function definitions
+filter_sc <- function(sc) {
+    sc <- PercentageFeatureSet(sc, pattern = "^MT-", col.name = "percent.mt")
+    sc <- PercentageFeatureSet(sc, pattern = "^RP[SL]", col.name = "percent.ribo")
+    
+    sc_filtered <- subset(x = sc, subset = (percent.mt <= 10) &
+                              (nFeature_RNA >= 1000 & nFeature_RNA <= 7000) &
+                              (nCount_RNA > 500) & (percent.ribo <= 40)
+    )
+    
+    this_counts <- GetAssayData(sc_filtered, slot = "counts")
+    nonzero_genes <- this_counts > 0
+    
+    # Keep genes whose expression is found in at least 5% of the sample
+    sample_cell_cutoff <- round(ncol(sc_filtered) / 100 * 5, digits = 0)
+    genes_to_keep <- Matrix::rowSums(nonzero_genes) >= sample_cell_cutoff
+    
+    new_filtered_sc <- CreateSeuratObject(
+        counts = this_counts[genes_to_keep, ],
+        meta.data = sc_filtered@meta.data
+    )
+
+    return(new_filtered_sc)
+}
+
+normalize_and_scale <- function(sc) {
+    sc <- Seurat::NormalizeData(sc,
+                                normalization.method = "LogNormalize",
+                                scale.factor = 10000
+                                )
+    sc <- Seurat::FindVariableFeatures(sc, selection.method = "vst")
+    sc <- Seurat::ScaleData(sc, features = rownames(sc))
+    return(sc)
+}
+
+#Add anotation to cells
+anot <- read.table(celltype,
+                   sep = "\t",
+                   header = T,
+                   row.names = 1
+                   )
+
+sc_Peng_PDAC@meta.data <- merge(sc_Peng_PDAC@meta.data,
+                                anot,
+                                by = "row.names",
+                                all.x = T
+                                )
+
+row.names(sc_Peng_PDAC@meta.data) <- sc_Peng_PDAC@meta.data$Row.names
+sc_Peng_PDAC@meta.data <- sc_Peng_PDAC@meta.data[,-1]
+
+# Change default ident for later split and QC
+sc_Peng_PDAC@meta.data$orig.ident <- sc_Peng_PDAC@meta.data$Sample
+Idents(sc_Peng_PDAC) <- "orig.ident"
+
+## Split the merged obj
+samples_list <- Seurat::SplitObject(object = sc_Peng_PDAC,
+                                    split.by = "orig.ident"
+                                    )
+
+## Filter cells
+filtered_sc <- lapply(samples_list, filter_sc)
+
+## Normalize and scale data
+filtered_sc <- lapply(filtered_sc, normalize_and_scale)
+
+# Get rid of "NULL" samples, there are no cells left in these
+filtered_sc[sapply(filtered_sc, is.null)] <- NULL
+
+## Seurat object
+saveRDS(object = filtered_sc,
+        file = where_to_save
+        )
