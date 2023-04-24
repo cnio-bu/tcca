9f8a61f35aeac43f9cf09533f51ac1a5ae80e82d
Santiago Garcia <s.g.martin94@gmail.com>
2023-04-24 12:44:02 +0200

    Add missing annotation to adrenalnb

--- src/preprocessing/workflow/scripts/adrenalnb_rui_chong_seurat.R
+++ src/preprocessing/workflow/scripts/adrenalnb_rui_chong_seurat.R
@@ -1,12 +1,75 @@
-library("tidyverse")
 library("Seurat")
+library("tidyverse")
 
 ## SNAKEMAKE I/O
-seurat_object_list <- snakemake@input[["object_list"]]
+object_list <- snakemake@input[["object_list"]]
+reference_gene_annotation <- snakemake@input[["reference_gene_annotation"]]
+tumor_data <- snakemake@input[["tumor_data"]]
+gland_data <- snakemake@input[["gland_data"]]
+
 where_to_save <- snakemake@output[["seurat_list"]]
 
-## Load the source file... big RData obj
-load(seurat_object_list)
+load(object_list)
+
+gene_annot <- readr::read_tsv(reference_gene_annotation)
+
+## Extract count mat
+mat <- human_NB_subset_tumor@assays$SCT@counts
+ensembl_genes <- rownames(mat)
+
+## Annotate to HGNC
+genes_to_keep <- gene_annot %>%
+select(symbol, ensembl_gene_id) %>%
+filter(ensembl_gene_id %in% ensembl_genes)
+
+gene_dict <- genes_to_keep %>%
+select(ensembl_gene_id, symbol) %>% 
+deframe()
+
+annotated_hugo <- gene_dict[ensembl_genes]
+
+## Remove NAs
+annotated_hugo <- annotated_hugo[!is.na(annotated_hugo)]
+
+mat_annot <- mat[names(annotated_hugo), ]
+rownames(mat_annot) <- annotated_hugo[rownames(mat_annot)]
+
+## Extract the original meta.data
+metadata <- human_NB_subset_tumor@meta.data
+
+## Free mem
+rm(human_NB_subset_tumor)
+gc()
+
+## Regenerate Seurat object
+full_seu <- Seurat::CreateSeuratObject(counts = mat_annot, meta.data = metadata)
+
+## Set idents to sample
+full_seu$orig.ident <- full_seu$Sample
+
+## Add missing metadata
+tumor_data <- readr::read_csv(tumor_data) %>%
+select(cellname, celltype) %>%
+as.data.frame()
+
+rownames(tumor_data) <- tumor_data$cellname 
+tumor_data$cellname <- NULL
+
+gland_data <- readr::read_csv(gland_data) %>% 
+select(cell_id, annotation) %>% 
+as.data.frame()
+
+rownames(gland_data) <- gland_data$cell_id
+gland_data$cell_id <- NULL 
+
+full_seu <- AddMetaData(full_seu, metadata = tumor_data)
+full_seu <- AddMetaData(full_seu, metadata = gland_data)
+
+
+## Split the merged obj
+samples_list <- Seurat::SplitObject(object = full_seu, 
+                                    split.by = "Sample"
+                                    )
 
 ## Function definitions
 filter_sc <- function(sc) {
@@ -43,22 +106,10 @@ normalize_and_scale <- function(sc) {
     return(sc)
 }
 
-## Reset idents
-human_NB_subset_tumor$old_ident <- human_NB_subset_tumor$orig.ident
-human_NB_subset_tumor$orig.ident <- human_NB_subset_tumor$Sample
-
-## Split the merged obj
-samples_list <- Seurat::SplitObject(object = human_NB_subset_tumor, 
-                                    split.by = "Sample")
-
-## Filter cells
 filtered_sc <- lapply(samples_list, filter_sc)
-
-## Normalize and scale data
 filtered_sc <- lapply(filtered_sc, normalize_and_scale)
 
-## Seurat object
 saveRDS(
     object = filtered_sc,
     file = where_to_save
-    )
+)
