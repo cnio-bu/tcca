6b7c3a6c0956262a6fb546ae2e1eea8ce00ee26d
SGMartin <s.g.martin94@gmail.com>
2023-06-02 13:17:32 +0200

    Fix malignant subset in brca bhupinder pal

--- src/preprocessing/workflow/scripts/brca_bhupinder_pal_beyondcell.R
+++ src/preprocessing/workflow/scripts/brca_bhupinder_pal_beyondcell.R
@@ -21,13 +21,13 @@ annotate_cell_cycle <- function(sc){
 
 # keep malignants cells only
 filter_malignants <- function(sc) {
-    if (sum(sc@meta.data$Celltype..major.lineage. == "Malignant") > 0) {
-        sc_filtered <- subset(x = sc, subset = Celltype..major.lineage. == "Malignant")
-        return(sc_filtered)
-    } else {
-        return(NULL)
-    }
-    
+  if (unique(sc$Tissue) != "Normal" & sum(sc@meta.data$Celltype..major.lineage. == "Malignant") > 0) {
+    sc_filtered <- subset(x = sc, subset = Celltype..major.lineage. == "Malignant")
+    return(sc_filtered)
+  } else {
+    return(NULL)
+  }
+  
 }
 
 ## Calculate bcscores sample wise, for each sample, for malignant pops. only
