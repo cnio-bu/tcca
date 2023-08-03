16d72d87a7744874ba76a1a3ee6debca16246a6d
SGMartin <s.g.martin94@gmail.com>
2023-08-03 10:22:45 +0200

    Fix NSCLC stefan salcher

--- src/preprocessing/workflow/scripts/nsclc_stefan_salcher_seurat.R
+++ src/preprocessing/workflow/scripts/nsclc_stefan_salcher_seurat.R
@@ -49,6 +49,13 @@ samples_to_filter <- c("Leader_Merad_2021_336", "Leader_Merad_2021_298")
 
 seu_list <- seu_list[!(names(seu_list) %in% samples_to_filter)]
 seu_list <- lapply(seu_list, filter_sc)
+
+## Get rid of NULLs samples w/o malignants left
+seu_list[sapply(seu_list, is.null)] <- NULL
+## In this datasets, some samples had very few cells, less than the minimum
+## for calculating variable features
+seu_list <- seu_list[sapply(seu_list, ncol) > 10]
+
 seu_list <- lapply(seu_list, normalize_and_scale)
 
 saveRDS(seu_list, where_to_save)
