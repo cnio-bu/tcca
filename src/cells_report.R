9747a1c2f8ccf2a21448cdd004c4fc8b69d93534
SGMartin <s.g.martin94@gmail.com>
2023-08-30 15:47:48 +0200

    Update reports

--- src/cells_report.R
+++ src/cells_report.R
@@ -2,7 +2,7 @@ library("tidyverse")
 library("patchwork")
 
 all_reports <- list.files(
-    path = "results/non_malignant_assessing_test/reports",
+    path = "results/reports/",
     full.names = TRUE
     )
 
@@ -85,237 +85,24 @@ full_report_annotated <- full_report %>%
         )
     )
         
-        
-        
-report_summary_arranged <- full_report_annotated %>%
-    group_by(cancer_type) %>%
-    mutate(
-        avg_cells_by_type = mean(malignants)
-    ) %>%
-    arrange(desc(avg_cells_by_type))
-   
-report_summary_arranged$cancer_type <- fct_reorder(
-    report_summary_arranged$cancer_type,
-    report_summary_arranged$avg_cells_by_type
-    )
-
-## plot averages
-avg_malignants_by_tumor <- ggplot(data = report_summary_arranged) + 
-    geom_boxplot(aes(y=cancer_type, x = malignants)) +
-    scale_x_continuous(n.breaks = 10) +
-    xlab(label = "Malignant cells") +
-    ylab(label = "") +
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) +
-    ggtitle(label = "Average proportion of malignant cells by sample and tumor type")
 
 
-report_summary_all_malignants <- full_report_annotated %>%
-    group_by(cancer_type) %>%
-    summarise(
-        n.malignants = sum(malignants),
-        n.cells = sum(cells),
-        n.samples = n()
-    )  %>%
-    arrange(n.malignants) %>%
-    mutate(
-        cancer_type = as_factor(cancer_type)
-    )
+## keep those with > 100 malignants
+samples_to_keep <- full_report_annotated %>%
+    filter(malignants >= 100)
 
-## plot totals
-malignants_by_tumor_total <- ggplot(data=report_summary_all_malignants, aes(x = n.malignants, y = cancer_type)) + 
-    geom_bar(stat = "identity") +
-    geom_text(stat="identity", aes(label = n.malignants), hjust = -.3) +
-    scale_x_continuous(n.breaks = 10, labels = scales::scientific_format()) +
-    xlab(label = "Malignant cells") +
-    ylab(label = "") +
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) +
-    ggtitle(label = "Total proportion of malignant cells by tumor type")
-
-ggsave(malignants_by_tumor_total,filename = "results/cells_by_tumor_type.png",
-       dpi = 300, height = 7, width = 14
-       )
-## plots by proportion
-report_summary_all_malignants <- report_summary_all_malignants %>%
-    mutate(
-        prop.total = n.malignants / sum(n.malignants),
-        prop.total = round(prop.total, digits = 3)
-    )
-
-
-## plot props
-malignants_by_tumor <- ggplot(data=report_summary_all_malignants, aes(x = prop.total, y = cancer_type)) + 
-    geom_bar(stat = "identity") +
-    scale_x_continuous(n.breaks = 18, labels = scales::percent_format()) +
-    xlab(label = "Malignant cells") +
-    ylab(label = "") +
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) +
-    ggtitle(label = "Total proportion of malignant cells by tumor type")
-
-
-report_summary_all_malignants <- full_report_annotated %>%
-    group_by(cancer_type) %>%
-    summarise(
-        n.malignants = sum(malignants),
-        n.cells = sum(cells),
-        n.samples = n(),
-        prop.cells_by_sample = n.malignants / n.samples
-    )  %>%
-    arrange(n.samples) %>%
-    mutate(
-        cancer_type = as_factor(cancer_type)
-    )
-
-## samples by tumor
-samples_by_tumor <- ggplot(data = report_summary_all_malignants, aes(x = n.samples, y = cancer_type)) +
-    geom_bar(stat = "identity") +
-    scale_x_continuous(n.breaks = 10) +
-    xlab(label = "Samples") +
-    ylab(label = "") +
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) +
-    ggtitle(label = "Samples by tumor type")
-
-
-ggsave(
-    plot = samples_by_tumor,
-    filename = "results/samples_by_tumor_type.png",
-    dpi = 300,
-    height = 9,
-    width = 16
-    )
-
-
-ggsave(
-    plot = malignants_by_tumor,
-    filename = "results/proportion_of_malignants_per_tumor.png",
-    dpi = 300,
-    height = 9,
-    width = 16
-)
-
-study_wise_report <- full_report_annotated %>%
-    group_by(cancer_type) %>%
-    summarise(
-        n.malignants = sum(malignants),
-        n.cells = sum(cells),
-        n.samples = n(),
-        n.studies = sum(length(unique(study)))
-    )  %>%
-    arrange(n.studies) %>%
-    mutate(
-        cancer_type = as_factor(cancer_type)
-    ) 
-
-## studies by tumor
-samples_by_tumor <- ggplot(data = study_wise_report, aes(x = n.studies, y = cancer_type)) +
-    geom_bar(stat = "identity") +
-    scale_x_continuous(n.breaks = 4) +
-    xlab(label = "Samples") +
-    ylab(label = "") +
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) +
-    ggtitle(label = "Studies by tumor type")
-
-
-ggsave(
-    plot = samples_by_tumor,
-    filename = "results/studies_per_tumor.png",
-    dpi = 300,
-    height = 9,
-    width = 16
-)
-
-prop_cells_tumor_dt <- full_report_annotated %>%
-    group_by(cancer_type) %>%
-    summarise(
-        n.malignants = sum(malignants),
-        n.cells = sum(cells),
-        n.samples = n(),
-        avg.mals.sample = round(n.malignants / n.samples, digits = 1)
-    )  %>%
-    arrange(avg.mals.sample) %>%
-    mutate(
-        cancer_type = as_factor(cancer_type)
-    )
-
-    
-
-## average prop. of cells by sample by tumor_type
-prop_cells_by_tumor <- ggplot(data = prop_cells_tumor_dt, aes(y = cancer_type, x = avg.mals.sample)) +
-    geom_bar(stat = "identity") +
-    geom_text(stat = "identity", aes(label = n.samples), hjust = -.3) +
-    scale_x_continuous(n.breaks = 10) +
-    xlab("Average proportion of malignant cells") +
-    ylab("") + 
-    theme_bw() +
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank()
-    ) 
-    
-
-ggsave(
-    plot = prop_cells_by_tumor,
-    filename = "results/prop_cells_by_tumor.png",
-    dpi = 100
-    )
-
-
-## test 
-summary_malignants <- full_report_annotated %>%
+cells_by_study <- samples_to_keep %>%
     group_by(study) %>%
     summarise(
         n.samples = n(),
-        n.malignants = sum(malignants)
+        n.cells = sum(malignants)
     )
 
 
-write.table(x = full_report_annotated, file = "results/all_samples_annotated.tsv")
+samples_by_study <- samples_to_keep %>%
+    group_by(study) %>%
+    summarise(
+        sample = sample
+    )
 
-## save only malignants
-
-full_report_malignant_annotated <- full_report_annotated %>%
-    filter(malignants > 0)
-
-
-write.table(x = full_report_malignant_annotated, file = "results/all_malignant_samples.tsv")
-
-cum_dist <- ggplot(full_report_malignant_annotated, aes(x = malignants, y = sample)) +
-    stat_ecdf(geom = "step") +
-    scale_y_continuous(n.breaks = 10) + 
-    scale_x_continuous(n.breaks = 20)
-
-
-
-database_filtered <- full_report_malignant_annotated %>%
-    filter(malignants >= 200)
+write.csv(x = samples_by_study, file = "results/all_samples_annotated.tsv")
