63cc83701a09c70d2f6f9fd5f538d989b0829967
SGMartin <s.g.martin94@gmail.com>
2023-11-23 13:09:02 +0100

    Update and organize repo for phase 2

--- src/beyondcell_pancancer/merge_studywise_bps.R
+++ src/beyondcell_pancancer/merge_studywise_bps.R
@@ -19,7 +19,7 @@ cbind.fill<-function(...){
         rbind(x, matrix(, n-nrow(x), ncol(x))))) 
 }
 
-all_mats <- all_mats[2:37]
+all_mats <- all_mats[2:36]
 
 mats <- map(
     all_mats,
@@ -49,9 +49,9 @@ all.meta <- list.files(
 
 
 meta.data <- all.meta %>%
-    map(read.table, row.names = 1) 
+    map(read.table, row.names = 1, header = TRUE) 
 
-meta.data[[27]]$patient <- meta.data[[27]]$orig.ident
+meta.data[[26]]$patient <- meta.data[[26]]$orig.ident
 
 for(i in c(1:length(all.meta))){
     print(i)
@@ -75,7 +75,7 @@ meta.data_full <-  meta.data  %>%
                  ) %>%
              mutate(
                  "sample" = as.character(sample),
-                 "patient" = as.character(patient)
+                 "patient" = as.character(patient),
              )) %>%
     bind_rows() %>%
     mutate(
@@ -114,310 +114,17 @@ meta.data_full_clinical <- meta.data_full %>%
             refined_tumor_site == "" ~ "Unknown",
             TRUE ~ refined_tumor_site
         )
+    ) %>%
+    mutate(
+        original_cell_id = gsub(
+            pattern = "\\.\\.\\..*$", ## annoying ... by seurat
+            replacement = "",
+            x = cell
+        ),
+        new_cell_id = c(1:ncol(mat))
     )
 
 write_tsv(
     x = meta.data_full_clinical, 
     file = "results/annotation/beyondcell_metadata_with_clinical.tsv"
     )
-
-meta.data_full_clinical <- meta.data_full_clinical %>%
-    as.data.frame()
-
-rownames(meta.data_full_clinical) <- meta.data_full_clinical$cell
-meta.data_full_clinical$cell <- NULL
-
-options(Seurat.object.assay.version = 'v5')
-options(future.globals.maxSize = 1e9)
-
-seu <- Seurat::CreateSeuratObject(
-    counts = mat,
-    assay = "RNA",
-    project = "beyondcell_pancancer",
-    meta.data = meta.data_full_clinical
-    )
-
-seu[["RNA"]]$data <- mat
-
-seu <- SketchData(
-    object = seu,
-    ncells = 50000,
-    method = "LeverageScore",
-    sketched.assay = "sketch"
-)
-
-DefaultAssay(seu) <- "sketch"
-
-
-## Extract matrix of leveraged cells for heatmap
-sketched_mat <- seu[["sketch"]]$data
-sketched_mat <- scale(x = sketched_mat, center = TRUE, scale = TRUE)
-
-## extract meta group annotation
-mt1 <- read_tsv(file = "results/modules/annotated/patients_non_treated_meta_groups.tsv") %>%
-    select(meta_community, signature, collapsed.MoAs) %>%
-    group_by(meta_community, signature) %>%
-    mutate(
-        n.repeats = n()
-    ) %>%
-    select(signature, meta_community, collapsed.MoAs) %>%
-    as.data.frame()
-
-samples_to_keep <- seu@meta.data %>%
-    filter(
-        sample_type == "p" & treated == "f"
-    ) %>%
-    filter(
-        study != "cell_lines_gabriella_kinker"
-    ) 
-
-cells_to_keep <- rownames(samples_to_keep)
-
-sketched_to_keep <- intersect(cells_to_keep, colnames(sketched_mat))
-
-
-rownames_annot <- data.frame(
-    "signature" = rownames(sketched_mat)
-) %>%
-    left_join(
-        y = mt1
-    ) %>%
-    arrange(meta_community) %>%
-    group_by(signature) %>%
-    slice_head() %>%
-    arrange(meta_community) %>%
-    mutate(
-        meta_community = as.character(meta_community),
-        meta_community = replace_na(meta_community, "None"),
-        meta_community = as_factor(meta_community)
-    ) %>%
-    as.data.frame()
-
-rownames(rownames_annot) <- rownames_annot$signature
-rownames_annot$signature <- NULL
-
-piti = c("#6cca8e","#8398dc","#ea95ae","#1dade6", "#ff5f76", "#ffb6b6","#fff154","#ba7fff","#ca9a8c", "#4b71e5",# "#cccccc",
-         "#ff6600","#add82f","#ff3333","#0dba3c", "#ff864c", "#c4ea94","#666699","#888888","#b8c0ba", "#d58aca","#6da753","#ca9a8c","#ff4430","#e06d23")
-
-has_comm <- rownames(rownames_annot[rownames_annot$meta_community != "None", ])
-smaller_sketch <- sketched_mat[has_comm, sketched_to_keep]
-
-subset_annot <- rownames_annot[rownames_annot$meta_community != "None", ]
-
-moa_cols <- piti[1:length(unique(subset_annot$collapsed.MoAs))]
-names(moa_cols) <- unique(subset_annot$collapsed.MoAs)
-
-
-meta_comms <- piti[1:length(unique(subset_annot$meta_community))]
-names(meta_comms) <- unique(subset_annot$meta_community)
-
-## Prepare heatmap annotations
-right_palette <- list(#"Mechanism of action" = moa_cols,
-    "Meta-community" = meta_comms
-)
-
-right_annotation <- ComplexHeatmap::HeatmapAnnotation(
-    #   "Mechanism of action" = rownames_annot$collapsed.MoAs,
-    "Meta-community" = subset_annot$meta_community,
-    which = "row",
-    show_annotation_name = FALSE,
-    col = right_palette
-)
-
-top_annot <- ComplexHeatmap::HeatmapAnnotation(
-    "Tumor of origin" = seu@meta.data[colnames(smaller_sketch), "refined_tumor_site"],
-    which = "col"
-)
-
-
-png(filename = "results/test.png", width = 24, height = 48, units = "in", res = 100)
-ComplexHeatmap::Heatmap(
-    matrix = smaller_sketch,
-    col = circlize::colorRamp2(c(-2,0,2), c("blue", "white", "red"), transparency = 0.3),
-    cluster_rows = FALSE,
-    cluster_row_slices = TRUE,
-    clustering_distance_rows = "pearson",
-    row_order = subset_annot[order(subset_annot$meta_community), "signature"],
-    row_split = subset_annot$meta_community,
-    cluster_columns = TRUE,
-    cluster_column_slices = TRUE,
-    clustering_distance_columns = "pearson",
-    column_split = 10,
-    show_row_names = FALSE,
-    show_column_names = FALSE,
-    right_annotation = right_annotation,
-    top_annotation = top_annot 
-)
-dev.off()
-
-
-# test
-smaller_sketch_dt <- as.data.frame(smaller_sketch) %>%
-    as_tibble(rownames = "signature") %>%
-    pivot_longer(
-        cols = all_of(2:27760),
-        names_to = "cell",
-        values_to = "enrichment"
-    )
-
-cell_level_meta <- seu@meta.data %>%
-    tibble::rownames_to_column("cell") %>%
-    select(cell, sample, study)
-
-
-smaller_sketch_dt_annot <- smaller_sketch_dt %>%
-    left_join(
-        y = cell_level_meta,
-        by = "cell"
-    ) %>%
-    left_join(
-        y = mt1,
-        by = "signature"
-    ) %>%
-    filter(
-        !is.na(meta_community)
-    ) %>%
-    group_by(cell, meta_community) %>%
-    summarise(
-        average_enrichment = median(enrichment)
-    )
-
-sketch_remat <- smaller_sketch_dt_annot %>%
-    pivot_wider(id_cols = meta_community, names_from = cell, values_from = average_enrichment) %>%
-    as.data.frame()
-
-rownames(sketch_remat) <- sketch_remat$meta_community
-sketch_remat$meta_community <- NULL
-sketch_remat <- as.matrix(sketch_remat)
-
-png(filename = "results/test_average_meta.png", width = 24, height = 48, units = "in", res = 100)
-ComplexHeatmap::Heatmap(
-    matrix = sketch_remat,
-    col = circlize::colorRamp2(c(-2,0,2), c("blue", "white", "red"), transparency = 0.3),
-  #  cluster_rows = FALSE,
-  #  cluster_row_slices = TRUE,
-  #  clustering_distance_rows = "pearson",
-  #  row_order = subset_annot[order(subset_annot$meta_community), "signature"],
-  #  row_split = subset_annot$meta_community,
-    cluster_columns = TRUE,
-    cluster_column_slices = TRUE,
-  #  clustering_distance_columns = "pearson",
-  #  column_split = 10,
-  #  show_row_names = FALSE,
-    show_column_names = FALSE,
-  #  right_annotation = right_annotation,
-  #  top_annotation = top_annot 
-)
-dev.off()
-
-
-
-
-## CLUSTERING
-
-### SEURAT MODES
-options(Seurat.object.assay.version = "v5")
-
-DefaultAssay(seu) <- "sketch"
-seu <- FindVariableFeatures(seu, nfeatures = 100, selection.method = "dispersion")
-
-seu <- ScaleData(seu)
-seu <- FindNeighbors(seu, features = VariableFeatures(seu))
-seu <- FindClusters(
-    seu,
-    resolution = 2,
-    method = "igraph",
-    group.singletons = TRUE,
-    cluster.name = "sketched.clusters"
-    )
-
-seu <- RunUMAP(
-    seu,
-    dims = NULL,
-    features = VariableFeatures(seu),
-    return.model = T,
-    assay = "sketch"
-)
-    
-
-nonsense_plot <- DimPlot(
-    seu,
-    label = T,
-    label.size = 3,
-    reduction = "umap",
-    pt.size = 1
-    ) + NoLegend()
-
-
-ggsave(
-    plot = nonsense_plot,
-    filename = "results/beyondcell_bp/bc_malignants_umap_default.png",
-    height = 7,
-    width = 7
-    )
-
-seu <- ProjectData(
-    object = seu,
-    assay = "RNA",
-    full.reduction = "pca.full",
-    sketched.assay = "sketch",
-    sketched.reduction = "pca",
-    umap.model = "umap",
-    dims = 1:50,
-    refdata = list(cluster_full = "seurat_clusters")
-)
-# now that we have projected the full dataset, switch back to analyzing all cells
-DefaultAssay(seu) <- "RNA"
-
-DimPlot(
-    seu, label = T,
-    label.size = 3,
-    reduction = "umap",
-    group.by = "cluster_full",
-    alpha = 0.1
-    ) + NoLegend()
-
-
-
-
-
-####### TEST WITH SKET
-sketched_mat_scaled <- scale(x = sketched_mat, center = TRUE, scale = TRUE)
-
-# calculate the variance for each gene
-rv <- matrixStats::rowVars(sketched_mat_scaled)
-
-library(ggplot2)
-
-sketched_rv_dist <- ggplot(data = as.data.frame(rv), aes(x=rv)) +
-    geom_density() +
-    geom_rug() +
-    scale_x_continuous(name = "Drug variance", n.breaks = 10) +
-    theme_bw() +
-    theme(
-        axis.line = element_line(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank()
-    )
-
-
-ggsave(
-    plot = sketched_rv_dist,
-    filename = "results/beyondcell_bp/pancancer_drug_variance.png",
-    height = 7,
-    width = 7,
-    dpi = 100
-    )
-
-## keep drugs with > 2 var
-drugs_to_keep <- rv[rv >= 2]
-
-sketched_mat_best <- sketched_mat_scaled[names(drugs_to_keep), ]
-
-# perform a PCA on the data in assay(x) for the selected genes
-sketched_mat_scaled[sketched_mat_scaled < 0] <- 0
-pca <- prcomp(sketched_mat_scaled, center = FALSE, scale. = FALSE)
-
-plot(pca$rotation[, 3], pca$rotation[, 4])
--- src/modules/graph_aggregation.R
+++ src/modules/graph_aggregation.R
@@ -4,64 +4,7 @@ library(tidyverse)
 moas <- readr::read_tsv(file = "reference/final_moas - Collapsed.tsv") %>%
     distinct(IDs, collapsed.MoAs, .keep_all = TRUE)
 
-clinical_database <- data.table::fread(
-    "results/annotation/clinical_metadata_v2_clean.tsv"
-) %>%
-    filter(
-        tumor_subtype != "predicted_tumour" ## Get rid of non clear tumoral stuff
-    )
-
-module_list <- list.files(
-    "results/modules",
-    full.names = TRUE,
-    recursive = TRUE,
-    pattern = "*_clusters.tsv"
-)
-
-## Load all modules
-modules <- module_list %>%
-    map(data.table::fread) %>%
-    bind_rows(.id = "sample_study")
-
-modules$sample_study <- module_list[as.numeric(modules$sample_study)]
-
-modules_annotated <- modules %>%
-    mutate(
-        sample_study = stringr::str_remove(
-            pattern = "results/modules/", string = sample_study)
-    ) %>%
-    separate(
-        col = sample_study,
-        into = c("study", "sample"),
-        sep = "/"
-    ) %>%
-    mutate(
-        sample = stringr::str_remove(string = sample, pattern = "_clusters.tsv")
-    )
-
-## Use right join to get rid of duplicate samples
-modules_annotated_clinical <- modules_annotated %>%
-    right_join(
-        y = clinical_database,
-        by = c("sample" = "sample", "study" = "study")
-    ) %>%
-    mutate(
-        metagroup = case_when(
-            treated == "f" & study != "cell_lines_gabriella_kinker" & sample_type == "p" ~ "patient_primary_untreated",
-            treated == "f" & study != "cell_lines_gabriella_kinker" & sample_type == "m" ~ "patient_metastatic_untreated",
-            treated == "t" & study != "cell_lines_gabriella_kinker" & sample_type == "p" ~ "patient_primary_treated",
-            treated == "t" & study != "cell_lines_gabriella_kinker" & sample_type == "m" ~ "patient_metastatic_treated",
-            study == "cell_lines_gabriella_kinker" ~ "cell_line",
-            TRUE ~ "other"
-        )
-    )
-
-write.table(
-    x = modules_annotated_clinical,
-    file = "results/annotation/modules_annotated_clinical.tsv",
-    sep="\t",
-    row.names = FALSE
-)
+modules_annotated_clinical <- read_tsv(file = "results/annotation/modules_annotated_clinical.tsv")
 
 ## Perform aggregation of metagroup 1
 modules_mt1 <- modules_annotated_clinical %>%
@@ -78,7 +21,8 @@ cancers_to_consider <- modules_mt1 %>%
     ) %>%
     pull(tumor_type)
 
-for(cancer in cancers_to_consider){
+
+for (cancer in cancers_to_consider) {
     print(paste0("Analyzing ", cancer))
     
     modules_mt_by_cancer <- modules_mt1 %>%
@@ -110,7 +54,7 @@ for(cancer in cancers_to_consider){
     
     print("Plotting distribution and cutoff")
     dist_plot_2 <- ggplot(data = dist_contribution) +
-        geom_boxplot(aes(x = bicluster, y= cumulative_info)) +
+        geom_boxplot(aes(x = bicluster, y = cumulative_info)) +
         geom_smooth(aes(x = sort(as.numeric(bicluster)), y = cumulative_info)) +
         geom_hline(yintercept = 0.75) +
         scale_y_continuous(labels = scales::percent, n.breaks = 10) +
@@ -146,8 +90,8 @@ for(cancer in cancers_to_consider){
         from = combinations$V1,
         to = combinations$V2
     ) %>%
-        separate(col = "from", into = c("sample_1", "bicluster_1"), sep = "_") %>%
-        separate(col = "to", into = c("sample_2", "bicluster_2"), sep = "_") %>%
+        separate(col = "from", into = c("sample_1", "bicluster_1"), sep = "_(?=[^_]+$)") %>%
+        separate(col = "to", into = c("sample_2", "bicluster_2"), sep = "_(?=[^_]+$)") %>%
         filter(sample_1 != sample_2) %>%
         rowwise() %>%
         mutate(
@@ -234,11 +178,11 @@ for(cancer in cancers_to_consider){
                 to = paste0(sample_2, "_", bicluster_2)
             ) %>%
             select(from, to, weight)
-        
+                
         g <- graph_from_data_frame(
             relations_filtered,
             directed = FALSE,
-            vertices=module_edges
+            vertices = module_edges
         )
         
         print("Fast greedy classif")
@@ -317,7 +261,7 @@ cancers_to_consider <- modules_mt2 %>%
 
 
 
-for(cancer in cancers_to_consider){
+for (cancer in cancers_to_consider) {
     print(paste0("Analyzing ", cancer))
     
     modules_mt_by_cancer <- modules_mt2 %>%
@@ -349,7 +293,7 @@ for(cancer in cancers_to_consider){
     
     print("Plotting distribution and cutoff")
     dist_plot_2 <- ggplot(data = dist_contribution) +
-        geom_boxplot(aes(x = bicluster, y= cumulative_info)) +
+        geom_boxplot(aes(x = bicluster, y = cumulative_info)) +
         geom_smooth(aes(x = sort(as.numeric(bicluster)), y = cumulative_info)) +
         geom_hline(yintercept = 0.75) +
         scale_y_continuous(labels = scales::percent, n.breaks = 10) +
