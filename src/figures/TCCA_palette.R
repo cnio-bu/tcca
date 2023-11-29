5c6150bf6af3f15e5c132186969e9b31b9689614
Laura Serrano-Ron <127123737+lserranor@users.noreply.github.com>
2023-11-29 15:52:42 +0100

    Add TCCA_palette.R

---
+++ src/figures/TCCA_palette.R
@@ -0,0 +1,83 @@
+## TCCA COLOR PALETTES
+
+# Tumor type x15 (14 + Other)
+tumor_sites_colors <- c(lung = "#87E3EF",
+                        brain = "#CA9A8C",
+                        bone_marrow= "#FFE072",
+                        breast = "#FF8FAB",
+                        esophagus = "#5780FE",
+                        skin = "#5E2D2C",
+                        pancreas = "#B47EB3",
+                        kidney = "#FF864C",
+                        colon = "#005D95",
+                        lymph_node = "#B6F884",
+                        liver = "#5DA271",
+                        pleural_effusion = "#159EA2",
+                        ovary = "#9C0D38",
+                        oral_cavity = "#FB6467",
+                        other= "#BBB9B7")
+
+# Treated/Untreated 
+treatment_colors <- c(treated = "#6ED1BC",
+                      untreated = "#D18B6E")
+
+# Adult/Pediatric 
+age_colors <- c(adult = "#7689DE",
+                pediatric = "#a9dce3")
+
+# Male/Female 
+sex_colors <- c(male = "#ec5c44",
+               female = "#1c8c8c")
+
+# Solid/Liquid 
+sl_colors <- c(solid = "#F6Bd60",
+                liquid = "#706695")
+
+# Primary/Met 
+pm_colors <- c(metastasis = "#C10044",
+               primary = "#F0BFD0")
+
+# Therapeutic clusters x5
+tcs_colors <- c("5"= "#33539E",
+                "4" = "#7FACD6",
+                "3" = "#BFB8DA",
+                "2" = "#E8B7D4",
+                "1" = "#A5678E")
+
+# Expression clusters x10
+ecs_colors <- c("10" ="#A7D676",
+                "9" = "#43978D",
+                "8" = "#90E2ED",
+                "7" = "#369CBB",
+                "6" = "#406792",
+                "5" = "#B46F9C",
+                "4" = "#D05B61",
+                "3" = "#FE7B47",
+                "2" = "#FFA72C",
+                "1" = "#FFBD71")
+# MoAs
+MoAs_colors <- c("#6cca8e", #This is the Pitiscale, but ffdd56 --> ffdd72
+                 "#8398dc",
+                 "#ea95ae",
+                 "#1dade6", 
+                 "#ff5f76", 
+                 "#ffb6b6",
+                 "#fff154",
+                 "#ba7fff",
+                 "#ffdd72", 
+                 "#4b71e5", 
+                 "#cccccc",
+                 "#ff6600",
+                 "#add82f",
+                 "#ff3333",
+                 "#0dba3c", 
+                 "#ff864c", 
+                 "#c4ea94",
+                 "#666699",
+                 "#888888",
+                 "#b8c0ba",
+                 "#d58aca",
+                 "#6da753",
+                 "#ca9a8c",
+                 "#ff4430",
+                 "#e06d23")
