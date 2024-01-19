c5f69e2a92eee0c329aea0283b4d201cfb93c6c6
SGMartin <s.g.martin94@gmail.com>
2024-01-19 11:15:19 +0100

    Finish AML analysis

--- src/aml/seurat_analysis.R
+++ src/aml/seurat_analysis.R
@@ -73,10 +73,10 @@ seu <- subset(seu, subset = GEO_ID != "PAWNPU_Dx_scRNA")
 
 seu <- SketchData(
     seu,
-    ncells = 30000,
+    ncells = 500,
     method = "LeverageScore",
     var.name = "leverage.score",
-    sketched.assay = "sketch_10k",
+    sketched.assay = "sketch_500",
     over.write = TRUE,
     seed = 120394
     )
@@ -84,6 +84,119 @@ seu <- SketchData(
 saveRDS(
     object = seu,
     file = "results/aml/seu.Rds",
-    destdir = "results/aml", 
-    overwrite = TRUE
 )
+
+DefaultAssay(seu) <- "sketch_500"
+
+seu <- FindVariableFeatures(seu)
+seu <- ScaleData(seu)
+seu <- RunPCA(seu)
+
+# integrate the datasets
+seu <- IntegrateLayers(
+    seu,
+    method = HarmonyIntegration,
+    new.reduction = "integrated.harmony"
+)
+
+# cluster the integrated data
+seu <- FindNeighbors(seu, reduction = "integrated.harmony")
+seu <- FindClusters(seu, resolution = 2, cluster.name = "integrated.clusters")
+
+# check integration
+seu <- RunUMAP(
+    seu,
+    dims = 1:30,
+    reduction = "integrated.harmony",
+    return.model = T,
+    verbose = F
+    )
+
+sketch_umap_sample <- DimPlot(
+    seu,
+    group.by = "gsm_id",
+    reduction = "umap",
+    label = FALSE
+    ) +
+    NoLegend()
+
+sketch_umap_major_ctypes <- DimPlot(
+    seu,
+    reduction = "umap",
+    group.by = "Classified_Celltype",
+    label = TRUE
+    ) +
+    NoLegend()
+
+sketch_umap_malignants <- DimPlot(
+    seu,
+    reduction = "umap",
+    group.by = "Malignant"
+    )
+
+sketch_umap_treatment <- DimPlot(
+    seu,
+    reduction = "umap",
+    group.by = "Subgroup"
+    )
+
+
+
+test <- CombinePlots(plots = list(sketch_umap_sample,
+                                  sketch_umap_major_ctypes,
+                                  sketch_umap_malignants,
+                                  sketch_umap_treatment)
+                     )
+
+ggsave(
+    filename = "results/aml/combined_aml_sketch_umaps_integrated.png",
+    plot = test,
+    dpi = 300,
+    height = 14, 
+    width = 20
+    )
+
+
+## Integrate back the full dataset
+# resplit the sketched cell assay into layers this is required to project the integration onto
+# all cells
+seu <- ProjectIntegration(
+    object = seu,
+    sketched.assay = "sketch_500",
+    assay = "RNA",
+    reduction = "integrated.harmony"
+    )
+
+seu <- RunUMAP(
+    object = seu, 
+    reduction = "integrated.harmony.full",
+    dims = 1:30,
+    reduction.name = "umap.full",
+    reduction.key = "UMAP_full_"
+    )
+
+saveRDS(object = seu, file = "results/aml/seu.Rds")
+
+DefaultAssay(seu) <- "RNA"
+
+## Merge all layers after integration and clustering
+seu <- JoinLayers(object = seu, assay = "RNA")
+
+## Cell cycle
+seu <- CellCycleScoring(
+    object = seu,
+    s.features = cc.genes$s.genes,
+    g2m.features = cc.genes$g2m.genes
+)
+
+## Filter and keep malignant cells only
+malignants <- subset(seu, subset = Malignant == "Malignant")
+saveRDS(object = malignants, file = "results/aml/malignants_seu.rds")
+
+## generate mat and meta for bc
+rm(seu)
+gc()
+bc_metadata <- malignants@meta.data
+seu_mat <- as(malignants[["RNA"]]$data, Class = "dgCMatrix")
+saveRDS(object = seu_mat, file = "results/aml/normalized_malignants_mat.rds")
+write.table(bc_metadata, file = "results/aml/malignants_annotation.tsv")
