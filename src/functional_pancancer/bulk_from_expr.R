dcaaaa134238cdcba7c6665c5b6e0ce1a464c9ed
SGMartin <s.g.martin94@gmail.com>
2024-02-26 16:21:42 +0100

    Add pseudobulk analysis

---
+++ src/functional_pancancer/bulk_from_expr.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(BPCells)
+library(Seurat)
+
+## Set options
+options(future.globals.maxSize = 1e9)
+options(Seurat.object.assay.version = 'v5')
+
+seu <- readRDS("results/lvl1/seu_lvl1_merged.Rds")
+mat <- open_matrix_dir(dir = "results/lvl1/pancancer_merged_mat")
+
+seu@assays$RNA$counts <- mat
+
+seu <- NormalizeData(seu)
+
+seu_bulk <- AggregateExpression(
+    object = seu,
+    slot = "counts",
+    return.seurat = T,
+    group.by = c("sample", "study")
+    )
+
+mat <- seu_bulk@assays$RNA@counts
+mat <- as.matrix(mat)
+
+write.table(x = mat, file = "results/functional/pancancer_pseudobulk.tsv")
