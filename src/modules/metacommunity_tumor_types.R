583086dd33e1f9b22b922b521837a1811ec70723
SGMartin <s.g.martin94@gmail.com>
2024-03-08 13:22:54 +0100

    Add metacommunities by tumor type

--- src/modules/metacommunity_tumor_types.R
+++ src/modules/metacommunity_tumor_types.R
@@ -25,7 +25,12 @@ bc <- AddModuleScore(
 )
 
 
-meta.data <- bc@meta.data
+## Somehow there are ~10 NA rows maybe after binding
+meta.data <- bc@meta.data %>%
+    filter(!is.na(sample)) %>%
+    mutate(
+        sample_study = paste0(sample, study, sep = "_")
+    )
 
 metacom_types <- meta.data %>%
     select(
@@ -36,163 +41,131 @@ metacom_types <- meta.data %>%
         sample_type,
         treated,
         study,
+        sample_study,
         metacom_untreated_1:metacom_untreated_6
         )
 
+## Export a reduced matrix for potential analyses
 write_tsv(
     x = metacom_types,
     file = "results/modules/annotated/pancancer_modules_mt1.tsv"
     )
 
-## Filter to keep MT1 samples
-metacom_types <- metacom_types %>%
-    filter(
-        sample_type == "p" &
-            treated == FALSE &
-            study != "cell_lines_gabriella_kinker"
-        ) %>%
-    pivot_longer(
-        cols = metacom_untreated_1:metacom_untreated_6,
-        names_to = "metacommunity",
-        values_to = "cell_enrichment"
-        ) %>%
-    group_by(sample, study, metacommunity) %>%
-    reframe(
-        sample_enrichment = median(cell_enrichment),
-        tumor_type = tumor_type
-    ) %>%
-    distinct()
-    
-
-metacom_types <- metacom_types %>%
-    group_by(tumor_type, metacommunity) %>%
-    mutate(n_samples = n()) %>%
-    filter(
-        n_samples >= 3
-    ) %>%
-    mutate(tumor_type = as_factor(tumor_type)) 
-
-metacom_summary <- metacom_types %>%
-    group_by(metacommunity) %>%
+## Aggregate all enrichment scores by sample
+metacom_sample_agg <- meta.data %>%
+    group_by(sample_study) %>%
     mutate(
-        pancancer_median = round(median(sample_enrichment), digits = 3)
-    ) %>%
-    group_by(tumor_type, metacommunity) %>%
-    reframe(
-        average_enrichment = round(median(sample_enrichment), digits = 3),
-        enrichment_disp = round(sd(sample_enrichment), digits = 3),
-        pancancer_median = pancancer_median
-    ) %>%
-    distinct() %>%
-    arrange(
-        desc(average_enrichment)
+        metacom_1_overall = sum(metacom_untreated_1),
+        metacom_2_overall = sum(metacom_untreated_2),
+        metacom_3_overall = sum(metacom_untreated_3),
+        metacom_4_overall = sum(metacom_untreated_4),
+        metacom_5_overall = sum(metacom_untreated_5),
+        metacom_6_overall = sum(metacom_untreated_6)
+        
     )
 
-
-write_tsv(
-    x = metacom_summary,
-    file = "results/modules/annotated/metacommunity_untreated_enrichment_by_cancer.tsv"
-    )
-
-
-## metacom types 2
-metacom_types_highest_cell <- meta.data %>%
+## Calculate highest metacom for each cell
+metacom_types_highest_cell <- metacom_sample_agg %>%
     select(
         cell,
+        new_cell_id,
         sample,
         tumor_type,
         sample_type,
         treated,
         study,
-        metacom_untreated_1:metacom_untreated_6
-    ) %>%
-    filter(
-        sample_type == "p" &
-            treated == FALSE &
-            study != "cell_lines_gabriella_kinker"
+        sample_study,
+        metacom_untreated_1:metacom_untreated_6,
     ) %>%
     pivot_longer(
         cols = metacom_untreated_1:metacom_untreated_6,
         names_to = "metacommunity",
         values_to = "cell_enrichment"
     ) %>%
-    group_by(cell) %>%
+    group_by(new_cell_id) %>%
     slice_max(order_by = cell_enrichment, n = 1)
 
 
 metacom_proportions <- metacom_types_highest_cell %>%
-    group_by(sample, study, metacommunity) %>%
-    summarise(
+    group_by(sample_study, metacommunity) %>%
+    reframe(
+        sample = sample,
+        study = study,
         n.cells = n()
     ) %>%
-    group_by(sample) %>%
+    distinct() %>%
+    group_by(sample_study) %>%
     mutate(
         n.total = sum(n.cells),
-        n.prop = n.cells / n.total * 100,
-        n.prop = round(n.prop, digits = 2)
+        n.prop = n.cells / n.total,
+        n.prop = round(n.prop, digits = 3)
     ) %>%
     arrange(desc(n.prop)) %>%
-    group_by(sample) %>%
+    group_by(sample_study) %>%
     mutate(
         best_metacom = head(metacommunity, n = 1)
     )
 
+metacom_sample_agg <- metacom_sample_agg %>%
+    select(sample, study, sample_study, metacom_1_overall:metacom_6_overall) %>%
+    distinct()
 
 ## add cancer types
+cancer_info <- meta.data %>%
+    select("sample_study", "sample_type", "treated", "tumor_type", "tumor_subtype") %>%
+    distinct()
+
 metacom_proportions_annotated <- metacom_proportions %>%
     left_join(
-        y = meta.data[, c("sample", "study", "sample_type", "tumor_type", "tumor_subtype")],
-        by = c("sample", "study")
+        y = cancer_info,
+        by = "sample_study"
     ) %>%
-    distinct() %>%
-    group_by(study, sample) %>%
-    mutate(
-        metacom_sd = sd(n.prop)
-    )
+    distinct()
 
-metacom_dispersion_ctype <- metacom_proportions_annotated %>%
-    group_by(tumor_type, metacommunity) %>%
-    summarise(
-        avg_prop = median(n.prop),
-        prop_sd = sd(n.prop),
-        tumor_subtype = tumor_subtype
-    )
+## sample sum information
+metacom_sums_by_sample <- metacom_sample_agg %>%
+    select(sample_study, metacom_1_overall:metacom_6_overall)
 
-metacom_densities <- ggplot(
-    data = metacom_dispersion_ctype,
-    aes(x = prop_sd, color = metacommunity)
-    ) +
-    geom_density(alpha = 0.3) +
-    scale_x_continuous(name = "Standard deviation of proportions") +
-    ylab("") +
-    scale_color_discrete(labels = paste("Metacommunity", c(1:6), sep = " ")) +
-    theme_minimal() +
-    ggtitle("Pancancer dispersion of cell proportions by metacommunity")
-
-
-metacom_averages <- ggplot(
-    data = metacom_dispersion_ctype,
-    aes(x = avg_prop, color = metacommunity)
-) +
-    geom_density(alpha = 0.3) +
-    scale_x_continuous(
-        name = "Average proportions",
-       # labels = scales::percent(100, scale = 1)
-        ) +
-    ylab("") +
-    scale_color_discrete(labels = paste("Metacommunity", c(1:6), sep = " ")) +
-    theme_minimal() +
-    ggtitle("Pancancer averages of cell proportions by metacommunity")
-
-
-## excelize for A.F.
+## excelize for A.F. and add sample enrichments
 metacom_proportions_annotated_wide <- metacom_proportions_annotated %>%
     pivot_wider(
-        id_cols = c("sample", "study", "tumor_type", "tumor_subtype"),
+        id_cols = c(
+            "sample",
+            "study",
+            "sample_study",
+            "tumor_type",
+            "tumor_subtype",
+            "sample_type",
+            "treated",
+            "best_metacom",
+            "n.total"
+            ),
         names_from = c("metacommunity"),
-        values_from = c("n.cells", "n.total", "n.prop", "metacom_sd")
-        )
+        values_from = c("n.cells", "n.prop")
+        ) %>%
+    left_join(
+        y = metacom_sums_by_sample,
+        by = "sample_study"
+    ) 
 
+## get the name of the metacom with the highest enrichment
+metacom_sample_agg_long <- metacom_sample_agg %>%
+    pivot_longer(
+        cols = metacom_1_overall:metacom_6_overall,
+        names_to = "metacommunity",
+        values_to = "enrichment"
+    ) %>%
+    group_by(sample_study) %>%
+    slice_max(order_by = enrichment, n = 1)
+
+metacom_proportions_annotated_wide <- metacom_proportions_annotated_wide %>%
+    left_join(
+        y = metacom_sample_agg_long[, c("sample_study", "metacommunity")],
+        by = "sample_study"
+    ) %>%
+    dplyr::rename(
+        "best_metacom_enrichment" = metacommunity
+    )
 
 write_tsv(
     x = metacom_proportions_annotated_wide,
@@ -218,7 +191,7 @@ metacom_sample_best_wide <- metacom_sample_best %>%
         id_cols = c("Tumor type"),
         names_from = "Metacommunity",
         values_from = "N.samples"
-    )
+    ) 
 
 write_tsv(
     x = metacom_sample_best_wide,
