fedcee1d1dbbf379b58bba083b7ded1bff86eb45
SGMartin <s.g.martin94@gmail.com>
2024-05-20 16:01:22 +0200

    Add pancan metacom study start

---
+++ src/modules/pancancer_metacommunities_study.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+pancancer_therapeutic_modules <- read.table(
+    file = "results/modules/annotated_hq/patient_primary_untreated_pancancer_communities.tsv",
+    sep = "\t",
+    header = TRUE
+    )
+
+moa_reference <- read.table(
+    "reference/final_moas - Collapsed.tsv",
+    sep = "\t",
+    header = TRUE
+    ) %>%
+    distinct(IDs, collapsed.MoAs, .keep_all = TRUE)
+
+
+tm_drugs <- pancancer_therapeutic_modules %>%
+    select(signature) %>%
+    distinct() %>%
+    mutate(
+        has_three_underscores = case_when(
+            stringr::str_count(string = tm_drugs$signature, pattern = "_") >= 3 ~ TRUE,
+            TRUE ~ FALSE
+        ),
+        signature_fixed = case_when(
+            has_three_underscores == TRUE ~ stringr::str_replace(string = tm_drugs$signature, pattern = "_", replacement = "-"),
+            TRUE ~ signature
+        )
+    ) %>%
+    separate(
+        col = signature_fixed,
+        into = c("drug_name", "study", "id"),
+        sep = "_"
+    )
+
+tm_drugs_moa <- tm_drugs %>%
+    left_join(
+        y = moa_reference[, c("original.IDs", "studies", "collapsed.MoAs")],
+        by = c("id" = "original.IDs", "study" = "studies")
+        ) %>%
+    select(signature, collapsed.MoAs) %>%
+    deframe()
+
+pancancer_therapeutic_modules$moa <- tm_drugs_moa[pancancer_therapeutic_modules$signature]
+
+write.table(x = pancancer_therapeutic_modules, file = "results/modules/annotated_hq/pancancer_communities_annotated_moa.tsv")
