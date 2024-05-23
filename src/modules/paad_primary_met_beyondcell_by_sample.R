adfec4c79a51bb9501a267175e5fe56279addd64
SGMartin <s.g.martin94@gmail.com>
2024-05-23 15:56:42 +0200

    Add PAAD by sample analysis

--- src/modules/paad_primary_met_beyondcell_by_sample.R
+++ src/modules/paad_primary_met_beyondcell_by_sample.R
@@ -3,8 +3,8 @@ library(beyondcell)
 mat <- readRDS("results/paad/normalized_seu_malignant_mat.rds")
 meta.data <- read.table("results/paad/seu_metadata_malignants.tsv")
 
-cells_to_keep <- meta.data[(meta.data$patient == "P02" & meta.data$sample_type == "m"), ]
-mat <- mat[, rownames(cells_to_keep)]
+p2_primary <- meta.data[(meta.data$patient == "P02" & meta.data$sample_type == "m"), ]
+mat <- mat[, rownames(p2_primary)]
 
 #gs <- GetCollection(SSc, include.pathways = FALSE)
 gs <- GenerateGenesets(
@@ -12,21 +12,99 @@ gs <- GenerateGenesets(
     perform.reversal = FALSE
 )
 
-bc <- bcScore(sc = mat, gs = gs, expr.thres = 0.1)k
+bc <- bcScore(sc = mat, gs = gs, expr.thres = 0.1)
 bc@normalized[is.na(bc@normalized)] <- 0
 bc <- bcRecompute(bc, slot = "normalized")
 
 ## load nFeatures from seuv5
-meta.data_p1 <- meta.data[(meta.data$patient == "P02" & meta.data$sample_type == "m"), ]
-bc@meta.data <- meta.data
+bc@meta.data <- p2_primary
 
 bc <- bcRegressOut(bc = bc, vars.to.regress = c("nFeature_RNA"))
 
-## export bcmat for seuv5
-saveRDS(object = bc@normalized, file = "results/paad/bc_normalized_mat_rgrss.rds")
+## get elbow
+bc <- bcUMAP(bc = bc, pc = NULL)
+bc <- bcUMAP(bc = bc, pc = 30, k.neighbors = 20, npcs = 50, res = 0.2)
 
-## sanity check
-bc <- bcUMAP(bc = bc, pc = 20, npcs = 20, res = 0.2)
-bcClusters(bc, idents = "bc_clusters_res.0.2")
+## get TC plot
+tcs <- bcClusters(bc = bc, idents = "bc_clusters_res.0.2")
 
 bcSignatures(bc, signatures = list(values = "Gemcitabine_GDSC_1190"))
+
+
+## simple heatmap
+library(ComplexHeatmap)
+
+## get top RV
+top_rv <- matrixStats::rowVars(x = bc@normalized)
+top_rv <- sort(top_rv, decreasing = TRUE)
+
+## get top RC
+top_rcs <- matrixStats::colVars(x = bc@normalized)
+top_rcs <- sort(top_rcs, decreasing = TRUE)
+
+## get top 10
+top_ten_drugs <- head(top_rv, n = 20)
+top_100_cells <- head(top_rcs, n = 500)
+
+
+## get modules
+mods <- read.csv(
+    file = "results/test.csv",
+    sep = "\t"
+    )
+
+drugs_to_keep <- mods[order(mods$tm), "drug"]
+drugs_to_keep <- intersect(drugs_to_keep, rownames(bc@normalized))
+
+
+test <- fabias(X = bc@normalized, p = 5)
+bis <- extractBic(fact = test) 
+
+heattop <- ComplexHeatmap::Heatmap(
+    matrix = t(scale(t(bc@normalized[names(top_ten_drugs), names(top_100_cells)]))),
+    col = circlize::colorRamp2(colors = c("blue", "white", "red"), breaks = c(-3, 0, 3)),
+    cluster_rows = TRUE,
+    cluster_columns = TRUE,
+    cluster_column_slices = TRUE,
+    clustering_distance_rows = "pearson",
+    clustering_distance_columns = "pearson",
+    #column_split = bc@meta.data$bc_clusters_res.0.2,
+    #cluster_row_slices = TRUE,
+    row_split = 3,
+    show_row_names = TRUE,
+    show_column_names = FALSE,
+    row_names_gp = gpar(fontsize = 8)
+)
+
+heat <- ComplexHeatmap::Heatmap(
+    matrix = scale(bc@normalized[drugs_to_keep, ], center = TRUE),
+    col = circlize::colorRamp2(colors = c("blue", "white", "red"), breaks = c(-3, 0, 3)),
+    cluster_rows = FALSE,
+    row_split = mods[mods$drug %in% drugs_to_keep, ]$tm,
+    #cluster_columns = TRUE,
+    #cluster_column_slices = TRUE,
+    clustering_distance_rows = "pearson",
+    clustering_distance_columns = "pearson",
+    #column_split = bc@meta.data$bc_clusters_res.0.2,
+    cluster_row_slices = TRUE,
+    show_row_names = TRUE,
+    show_column_names = FALSE,
+    row_names_gp = gpar(fontsize = 6)
+)
+
+heat2 <- ComplexHeatmap::Heatmap(
+    matrix = cor(t(bc@normalized[drugs_to_keep, ])),
+    col = circlize::colorRamp2(colors = c("blue", "white", "red"), breaks = c(-1, 0, 1)),
+    cluster_rows = FALSE,
+    row_split = mods[mods$drug %in% drugs_to_keep, ]$tm,
+    column_split = mods[mods$drug %in% drugs_to_keep, ]$tm,
+    #cluster_columns = TRUE,
+    #cluster_column_slices = TRUE,
+    clustering_distance_rows = "pearson",
+    clustering_distance_columns = "pearson",
+    #column_split = bc@meta.data$bc_clusters_res.0.2,
+    cluster_row_slices = TRUE,
+    show_row_names = TRUE,
+    show_column_names = FALSE,
+    row_names_gp = gpar(fontsize = 6)
+)
