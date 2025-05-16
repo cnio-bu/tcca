f537515048fa2238f8e47ffef1b5036ad96655d3
MariaGBermejo <mariagonzb@gmail.com>
2025-05-16 18:15:06 +0200

    update sbash script to run nmf per sample or subclone

--- src/pancancer_analyses/functional_pancancer/run_nmf_study&subclone.sh
+++ src/pancancer_analyses/functional_pancancer/run_nmf_study&subclone.sh
@@ -6,20 +6,20 @@ studies=($(find /storage/scratch01/shared/projects/bc-meta/single_cell/seurat/v5
 # Loop through studies and submit jobs with specific memory allocation
 for study in "${studies[@]}"; do
     if [[ "$study" == "nsclc_stefan_salcher"  || "$study" == "cell_lines_gabriella_kinker" ]]; then
-        sbatch --job-name=nmf \
+        sbatch --job-name=nmf_subclone \
                --output=logs/nmf_%A_%a.out \
                --error=logs/nmf_%A_%a.err \
                --time=05:00:00 \
                --mem=150G \
                --cpus-per-task=1 \
-               --wrap="Rscript ./compute_nmf_subclone_wise.R $study"
+               --wrap="Rscript ./compute_nmf_subclone.R $study"
     else
-        sbatch --job-name=nmf \
+        sbatch --job-name=nmf_subclone \
                --output=logs/nmf_%A_%a.out \
                --error=logs/nmf_%A_%a.err \
                --time=05:00:00 \
                --mem=20G \
                --cpus-per-task=1 \
-               --wrap="Rscript ./compute_nmf_subclone_wise.R $study"
+               --wrap="Rscript ./compute_nmf_subclone.R $study"
     fi
 done
