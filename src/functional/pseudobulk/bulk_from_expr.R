ef1e69a4b9e631f88447d0d02a76abafece42b94
MariaGBermejo <mariagonzb@gmail.com>
2025-11-13 11:31:55 +0100

    update pseudobulk computation at sample level

--- src/functional/pseudobulk/bulk_from_expr.R
+++ src/functional/pseudobulk/bulk_from_expr.R
@@ -6,24 +6,20 @@ library(Seurat)
 options(future.globals.maxSize = 1e9)
 options(Seurat.object.assay.version = 'v5')
 
-seu <- readRDS("results/lvl1/seu_lvl1_merged.Rds")
-mat <- open_matrix_dir(dir = "results/lvl1/pancancer_merged_mat")
-
-seu@assays$RNA$counts <- mat
-
-## Remove preneoplastic brca
-seu <- subset(seu, subset = study != "brca_bhupinder_pal" | tumor_subtype != "predicted_tumour")
+setwd("/storage/scratch01/shared/projects/bc-meta/single_cell/seurat/v5")
+seu <- readRDS("lvl1/seu_lvl1.rds")
+seu <- JoinLayers(seu)
 
 seu <- NormalizeData(seu)
-
+seu$study_sample <- paste(seu$study, seu$sample, sep = "_")
 seu_bulk <- AggregateExpression(
     object = seu,
     slot = "counts",
     return.seurat = T,
-    group.by = c("sample", "study")
+    group.by = c("study_sample")
     )
 
-mat <- seu_bulk@assays$RNA@counts
+mat <- seu_bulk[["RNA"]]$counts
 mat <- as.matrix(mat)
 
-write.table(x = mat, file = "results/functional/pancancer_pseudobulk.tsv")
+write.table(x = mat, file = "lvl1/pancancer_pseudobulk.tsv")
