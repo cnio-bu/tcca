c0648988006205dbce8042ca0993363f1f68626e
Laura Serrano-Ron <127123737+lserranor@users.noreply.github.com>
2025-12-04 11:05:18 +0100

    Update resource allocation and paths in sketch

--- src/scevan/8_sketch_rds.R
+++ src/scevan/8_sketch_rds.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
-#sbatch -c 20 --job-name=sketch -o log_sketch.txt -e error_sketch.txt --mem=200G -t 1140 --wrap "Rscript 8_sketch_rds.R"
-#sbatch -c 20 --job-name=sketch -o log_sketch.txt -e error_sketch.txt --mem=200G -t 1140 --dependency=afterok:4381416 --wrap "Rscript 8_sketch_rds.R"
+#sbatch -c 20 --job-name=sketch -o log_sketch.txt -e error_sketch.txt --mem=30G -t 200 --wrap "Rscript 8_sketch_rds.R"
+#sbatch -c 20 --job-name=sketch -o log_sketch.txt -e error_sketch.txt --mem=30G -t 200 --dependency=afterok:4658433 --wrap "Rscript 8_sketch_rds.R"
 
 library(BPCells)
 library(Matrix)
@@ -12,21 +12,21 @@ library(dplyr)
 library(tibble)
 library(Seurat)
 library(SeuratObject)
-library(SeuratDisk)
-library(Azimuth)
+#library(SeuratDisk)
 
 setwd("/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata")
-seu <- readRDS("full_genes_copynumber_1layer/full_genes_copynumber.rds")
+seu <- readRDS("full_genes_copynumber_lvl2.rds")
 
 seu <- NormalizeData(seu)
 seu[["RNA"]]$data <- seu[["RNA"]]$counts
-seu <- FindVariableFeatures(seu)
-seu <- SketchData(
-  object = seu,
-  ncells = 50000,
-  method = "LeverageScore",
-  sketched.assay = "sketch"
-)
+seu <- FindVariableFeatures(seu, assay = "RNA", nfeatures = 2000, layer="data")
+
+#seu <- SketchData(
+#  object = seu,
+#  ncells = 50000,
+#  method = "LeverageScore",
+#  sketched.assay = "sketch"
+#)
 
 #sketch_mat <- as.matrix(seu[["sketch"]]$counts)
 #sketch_mat <- Matrix(sketch_mat, sparse = T)
@@ -36,13 +36,13 @@ seu <- SketchData(
 #  dir = "/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/sketch_mat_cnv",
 #  overwrite = TRUE)
 
-sketch_metadata <- seu@meta.data
-sketch_metadata <- sketch_metadata[rownames(sketch_metadata) %in% colnames(seu[["sketch"]]$counts),]
+#sketch_metadata <- seu@meta.data
+#sketch_metadata <- sketch_metadata[rownames(sketch_metadata) %in% colnames(seu[["sketch"]]$counts),]
 
-write.table(sketch_metadata, 
-  "/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/sketch_mat_metadata.tsv",
-  row.names = T, 
-  sep = "\t")
+#write.table(sketch_metadata, 
+#  "/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/sketch_mat_metadata.tsv",
+#  row.names = T, 
+#  sep = "\t")
 
 
 DefaultAssay(seu) <- "RNA"
@@ -58,7 +58,7 @@ sketch_metadata <- seu@meta.data
 sketch_metadata <- sketch_metadata[rownames(sketch_metadata) %in% colnames(seu[["sketch_5k"]]$counts),]
 
 write.table(sketch_metadata, 
-  "/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/sketch_mat_metadata_5k.tsv",
+  "/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/sketch_mat_metadata_5k_lvl2.tsv",
   row.names = T, 
   sep = "\t")
 
@@ -68,14 +68,12 @@ setwd("/storage/scratch01/shared/projects/bc-meta/single_cell/cna_metadata/")
 
 saveRDS(
   object = seu,
-  file = "full_genes_copynumber.rds",
-  destdir = "full_genes_copynumber_sketch",
-  relative = TRUE
+  file = "full_genes_copynumber_lvl2.rds"
+#  destdir = "full_genes_copynumber_sketch",
+#  relative = TRUE
 )
 
+## Save sketched matrix already scaled and normalized for plotting
+sketched_mat <- as.matrix(seu[["sketch_5k"]]$data)
 
-
-
-
-
-
+saveRDS(sketched_mat, file = "full_genes_copynumber_sketch_5k_scaled_normcounts_mtx_lvl2.rds")
